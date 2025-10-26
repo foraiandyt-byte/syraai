3e1562d13f1377f851d1d64c7969a9fa2ed513b7
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-26 18:13:18 +0530

    Change chat message role from 'assistant' to 'Syrah'

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if user_input := st.chat_input("Type your message..."):
 
     st.session_state.messages.append({"role": "assistant", "content": bot_reply})
 
-    with st.chat_message("assistant"):
+    with st.chat_message("Syrah"):
         st.write(bot_reply)
     
     client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  # ⬅️ Replace with your real Gemini API key
