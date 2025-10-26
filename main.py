fbf73131e74c91346164fb305d9f9e7665043e10
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-26 10:58:26 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import google.genai
+
+# --- SETUP ---
+genai.configure(api_key="AIzaSyArI5AG69nKu8eIhUiPWsCORf-JKJcLyXA")
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+# --- STREAMLIT UI ---
+st.set_page_config(page_title="Syrah AI", page_icon="")
+st.title("🤖 Syrah.ace")
+st.write("AI coding expert (ace)!")
+
+# Chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display old messages
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+# Get user input
+user_message = st.chat_input("Ask anything about coding...")
+
+if user_message:
+    # Show user message
+    st.session_state.messages.append({"role": "user", "content": user_message})
+    st.chat_message("user").write(user_message)
+
+    # AI Response
+    response = model.generate_content(user_message)
+    bot_reply = response.text
+
+    # Show bot message
+    st.session_state.messages.append({"role": "assistant", "content": bot_reply})
+    st.chat_message("assistant").write(bot_reply)
