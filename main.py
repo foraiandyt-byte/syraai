1faa0d83489f76d5f8717dae842d6efdf18e0a3f
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-26 11:14:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,6 @@ import streamlit as st
 import google.genai as genai
 
 client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  
-while True:
-    user_input = input("You: ")
-
-    if user_input.lower() in ["exit", "quit"]:
-        print("Chatbot: Goodbye! 👋")
-        break
-
-    try:
-        # Generate response using Gemini model
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",   # You can change to "gemini-1.5-pro" for deeper answers
-            contents=user_input
-        )
-
-        print("Chatbot:", response.output_text)
-
-    except Exception as e:
-        print(f"⚠️ Error: {e}")
 
 st.set_page_config(page_title="Syrah AI", page_icon="")
 st.title("🤖 Syrah.ace")
