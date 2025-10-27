b01531ee89dc613d88d8b50e6521ede30b437edb
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 20:39:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ user_input = st.chat_input
 client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw") 
 from google import genai
 client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw") 
-model = genai.GenerativeModel("gemini-2.5-flash")
-response = model.generate_content(user_input)
+response = client.generate_content(
+  model=("gemini-2.5-flash")
+  content=user_input
 bot_reply = response.text
 (f"Bot: {bot_reply}")
 print(bot_reply)
