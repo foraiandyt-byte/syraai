befba70349e2f013faa0048d2376a2752316c288
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 19:42:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ prompt = st.chat_input(
 )
 if prompt and prompt.text:
     st.markdown(prompt.text)
-if prompt and prompt["files"]:
-    st.image(prompt["files"][0])
 st.session_state.chat_history.append(f"You: {prompt}")
     
 client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  
