1346c0c7da5ac92f16babaf61525607851f92d6a
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 20:02:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from google import genai
 
-st.title("Syrah.Ace")
-st.subheader("Made by Team Syrah (VBB 2025)! Ace stands for AI Coding Expert")
+# Initialize chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+st.title("Neuro")
+st.subheader("Made by Team Syrah (VBB 2025)! ace stands for AI coding Tutor")
 user_input = st.chat_input("Type your message:")
-
-st.session_state=(f"You: {user_input}")   
-client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  
-
+if st.button("Send") and chat_input:
+    st.session_state.chat_history.append(f"You: {user_input}")
+client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  # ⬅️ Replace with your real Gemini API key   
 try:
         response = client.models.generate_content(
             model="gemini-2.5-flash",
@@ -16,7 +19,6 @@ try:
         bot_reply = response.text
 except Exception as e:
         bot_reply = f"Error: {e}"
-        st.session_state=(f"Bot: {bot_reply}")
-
-for msg in st.session_state:
+    st.session_state.chat_history.append(f"Bot: {bot_reply}")
+for msg in st.session_state.chat_history:
     st.write(msg)
