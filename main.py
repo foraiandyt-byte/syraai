ea45612a3c7000ef0ef1437675839134dce4325e
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 20:37:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ from google import genai
 st.title("Syrah")
 st.subheader("Made by Team Syrah (VBB 2025)! ace stands for AI coding Tutor")
 user_input = st.chat_input(
-    "Say something and/or attach an image",
-    accept_file=True,
-    file_type=["jpg", "jpeg", "png"],
-)
-if user_input and user_input.text:
-    st.markdown(user_input.text)
-if user_input and user_input["files"]:
-    st.image(user_input["files"][0])
     (f"You: {user_input}")
     client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw") 
     from google import genai
