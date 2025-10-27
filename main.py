ebed77d91c7c2c2c0b6a6c63eac1c8ab2c5a07a3
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 20:38:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from google import genai
 st.title("Syrah")
 st.subheader("Made by Team Syrah (VBB 2025)! ace stands for AI coding Tutor")
 user_input = st.chat_input
-    (f"You: {user_input}")
-    client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw") 
-    from google import genai
-    client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw") 
-    model = genai.GenerativeModel("gemini-2.5-flash")
-    response = model.generate_content(user_input)
-    bot_reply = response.text
-    (f"Bot: {bot_reply}")
-    print(bot_reply)
+(f"You: {user_input}")
+client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw") 
+from google import genai
+client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw") 
+model = genai.GenerativeModel("gemini-2.5-flash")
+response = model.generate_content(user_input)
+bot_reply = response.text
+(f"Bot: {bot_reply}")
+print(bot_reply)
