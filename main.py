5e9391f97c9f1ff0785fa24be473f290e02c6e1b
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 20:08:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from google import genai
 st.title("Neuro")
 st.subheader("Made by Team Syrah (VBB 2025)! ace stands for AI coding Tutor")
 user_input = st.chat_input("Type your message:")
-if st.button("Send") and chat_input:
-    (f"You: {user_input}")
-    client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  # ⬅️ Replace with your real Gemini API key   
-try:
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=user_input
+(f"You: {user_input}")
+client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  # ⬅️ Replace with your real Gemini API key   
+response = client.models.generate_content(
+model="gemini-2.5-flash",
+contents=user_input
         )
-        bot_reply = response.text
-except Exception as e:
-        bot_reply = f"Error: {e}"
-        (f"Bot: {bot_reply}")
+bot_reply = response.text
+(f"Bot: {bot_reply}")
 print(bot_reply)
