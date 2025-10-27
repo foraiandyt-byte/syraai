f145fa621d36963aacb785746ded90d96ef9e49e
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 20:09:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ user_input = st.chat_input("Type your message:")
 (f"You: {user_input}")
 client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  # ⬅️ Replace with your real Gemini API key   
 response = client.models.generate_content(
-model="gemini-2.5-flash",
-contents=user_input
+  model="gemini-2.5-flash",
+  contents=user_input
         )
 bot_reply = response.text
 (f"Bot: {bot_reply}")
