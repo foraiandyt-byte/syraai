554b17897fb2bdb635e8ff6dd3a6018c911ba5ed
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 18:57:47 +0530

    Change image display from first to second file

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ prompt = st.chat_input(
 if prompt and prompt.text:
     st.markdown(prompt.text)
 if prompt and prompt["files"]:
-    st.image(prompt["files"][0])
+    st.image(prompt["files"][1])
 
 if st.button("Send") and user_input:
     
