e455437ab6132ad8c68ffc8acfa4cd0a4a3f75e3
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 20:23:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ try:
       model="gemini-2.5-flash",
       contents=user_input
         )
-bot_reply = response.text
-(f"Bot: {bot_reply}")
-print(bot_reply)
+except:
+    bot_reply = response.text
+    (f"Bot: {bot_reply}")
+    print(bot_reply)
