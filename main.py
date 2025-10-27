3cb6479f0712f56979a4f85ecca9b94e57694b68
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 20:11:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from google import genai
 
 st.title("Neuro")
 st.subheader("Made by Team Syrah (VBB 2025)! ace stands for AI coding Tutor")
-user_input = st.chat_input("Type your message:")
+user_input = st.chat_input(
+    "Say something and/or attach an image",
+    accept_file=True,
+    file_type=["jpg", "jpeg", "png"],
+)
+if prompt and prompt.text:
+    st.markdown(prompt.text)
+if prompt and prompt["files"]:
+    st.image(prompt["files"][0])
 (f"You: {user_input}")
 client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  # ⬅️ Replace with your real Gemini API key   
 response = client.models.generate_content(
