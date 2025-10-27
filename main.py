dded3aa9b6e2a49766ceddb3c1a3b76364f13b95
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-27 19:53:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import streamlit as st
 from google import genai
-from PIL import Image
+
 # Initialize chat history
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-st.title("Syrah.ace")
-st.subheader("AI coding expert")
-user_input = st.chat_input(
-    "Say something and/or attach an image",
-)
+st.title("Syrah.Ace")
+st.subheader("Made by Team Syrah (VBB 2025)! Ace stands for AI Coding Expert")
+user_input = st.chat_input("Type your message:")
 st.session_state.chat_history.append(f"You: {user_input}")
     
-client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  
-try:
+    client = genai.Client(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")  
+
+    try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",  
+            model="gemini-2.5-flash",
             contents=user_input
         )
+        bot_reply = response.text
+    except Exception as e:
+        bot_reply = f"Error: {e}"
 
-        print("Chatbot:", response.output_text)
-except Exception as e:
-        print(f"⚠️ Error: {e}")
+    st.session_state.chat_history.append(f"Bot: {bot_reply}")
+
+for msg in st.session_state.chat_history:
+    st.write(msg)
